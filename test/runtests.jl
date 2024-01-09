0b1dd61b91065fbe2d035e989cc4c22f9634797e
Tamás K. Papp <tkpapp@gmail.com>
2024-01-09 16:56:07 +0100

    add  more test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,21 @@
 using FlatViews
 using Test
 
-x = (a = 1, b = [2.0, 3.0], d = (4, 5f0))
-v = @inferred flatten(x)
-y = @inferred reconstruct_like(x, v)
-@test x == y
+@testset "inferred roundtrip" begin
+    x = (a = 1, b = [2.0, 3.0], d = (4, 5f0))
+    v = @inferred flatten(x)
+    @test v == 1:5
+    y = @inferred reconstruct_like(x, v)
+    @test x == y
+end
+
+@testset "errors" begin
+    x = (a = 1, b = 2)
+    @test_throws BoundsError flatten_into!(zeros(1), x)
+    @test_throws DimensionMismatch flatten_into!(zeros(3), x)
+    @test_throws BoundsError reconstruct_like(x, zeros(1))
+    @test_throws DimensionMismatch reconstruct_like(x, zeros(3))
+end
 
 using JET
 @testset "static analysis with JET.jl" begin
